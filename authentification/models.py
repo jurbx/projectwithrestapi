9f998b9f309f9e6b01b84abeec80d3787e58162c
jurbx <drimskiy.daniki@gmail.com>
2022-02-09 20:51:59 +0200

    1

--- authentification/models.py
+++ authentification/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.core.validators import URLValidator
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
-from django.urls import reverse
 from django.utils import timezone
 from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
